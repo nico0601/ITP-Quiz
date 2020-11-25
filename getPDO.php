ba84a3f2fc02072e4df16907a46bde20b441ad2f
Christoph Samuel <chsamuel@gmx.at>
2020-11-25 10:33:01 +0100

    changed pdo-passwd to from "root" to ""

--- getPDO.php
+++ getPDO.php
@@ -16,7 +16,7 @@ function getPDO()
     $host = "localhost";
     $db = "quiz";
     $user = "root";
-    $passwd = "root";
+    $passwd = "";
 
     try {
         $pdo = new PDO("mysql:host=$host;dbname=$db", "$user", "$passwd");
