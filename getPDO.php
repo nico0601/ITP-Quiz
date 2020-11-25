ebdd87d9e7b0183b132433eb39a4595e07d15f21
Christoph Samuel <chsamuel@gmx.at>
2020-11-25 10:28:04 +0100

    edited getPDO() to local and server-based

--- getPDO.php
+++ getPDO.php
@@ -1,10 +1,22 @@
 <?php
 function getPDO()
 {
+//    $host = "localhost";
+//    $db = "quiz_db";
+//    $user = "quiz_admin";
+//    $passwd = "quiz";
+//
+//    try {
+//        $pdo = new PDO("mysql:host=$host;dbname=$db", "$user", "$passwd");
+//    } catch (PDOException $e) {
+//        echo 'Verbindung fehlgeschlagen';
+//    }
+//    return $pdo;
+
     $host = "localhost";
-    $db = "quiz_db";
-    $user = "quiz_admin";
-    $passwd = "quiz";
+    $db = "quiz";
+    $user = "root";
+    $passwd = "root";
 
     try {
         $pdo = new PDO("mysql:host=$host;dbname=$db", "$user", "$passwd");
