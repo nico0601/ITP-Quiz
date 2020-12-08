06bd01032179c4b4b2c38b860512b81c878426c2
Christoph Samuel <chsamuel@gmx.at>
2020-12-08 15:46:54 +0100

    added responsive-layout

--- Admin-Login/index4.php
+++ Admin-Login/index4.php
@@ -5,17 +5,8 @@
 <head>
     <meta charset="UTF-8" name="viewport" content="width=device-width">
     <title>Adminpage</title>
-    <!--    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css"-->
-    <!--          integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">-->
-    <!--    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"-->
-    <!--            integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj"-->
-    <!--            crossorigin="anonymous"></script>-->
-    <!--    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js"-->
-    <!--            integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx"-->
-    <!--            crossorigin="anonymous"></script>-->
     <link rel="stylesheet" type="text/css" href="./style4.css">
     <link rel="icon" href="../Logos/Schwarzer%20Hintergrund/Logo.png">
-    <!--    <script language="javascript" src="js4.js" defer></script>-->
 </head>
 <body>
 
@@ -84,7 +75,7 @@ ENDE;
 
             echo "</div>";
 
-            echo "<input id='senden' name='frageID-4' value='$frageID' type='submit'>";
+            echo "<button id='senden' name='frageID-4' value='$frageID' type='submit'>Senden</button>";
 
             ?>
 
