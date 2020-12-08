bc106123b8a13f6c63d5f14b424587bf25971e03
Christoph Samuel <chsamuel@gmx.at>
2020-12-08 16:42:41 +0100

    instead of inputs there are textareas

--- Admin-Login/index4.php
+++ Admin-Login/index4.php
@@ -5,6 +5,14 @@
 <head>
     <meta charset="UTF-8" name="viewport" content="width=device-width">
     <title>Adminpage</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css"
+          integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"
+            integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj"
+            crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js"
+            integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx"
+            crossorigin="anonymous"></script>
     <link rel="stylesheet" type="text/css" href="./style4.css">
     <link rel="icon" href="../Logos/Schwarzer%20Hintergrund/Logo.png">
 </head>
@@ -31,7 +39,7 @@
                 }
             }
 
-            $fragen = $fragen[$frageID-1];
+            $fragen = $fragen[$frageID - 1];
 
             $sql = getPDO()->prepare("SELECT * FROM antwort WHERE fk_pk_frage_id = ?");
             $sql->execute(array($frageID));
@@ -43,31 +51,35 @@
             $antwort4 = isset($antworten[3]) ? $antworten[3] : null;
 
             $bearbeiten = <<<ENDE
-            <input type="text" id="fragenName" name="frage-4" placeholder="Ihre Frage..." value="$fragen[1]" autofocus required>
+            <div class="md-form">
+                <textarea id="fragenName" class="md-textarea form-control" rows="2" name="frage-4" placeholder="Frage..." autofocus required>$fragen[1]</textarea>
+            </div>
             <div id="antworten">
-                <div id="antwort1feld">
-                    <input type="text" class="antwort" id="antwort1" name="antwort1-4" value="$antwort1[1]" placeholder="Antwort 1..."
-                           required>
+                <div id="antwort1feld" class="md-form">
+                    <textarea id="antwort1" class="md-textarea form-control"rows="2" placeholder="Antwort..."  name="antwort1-4""
+                           required>$antwort1[1]</textarea>
                 </div>
-                <div id="antwort2feld">
-                    <input type="text" class="antwort" id="antwort2" name="antwort2-4" value="$antwort2[1]" placeholder="Antwort 2..."
-                           required>
+                <div id="antwort2feld" class="md-form">
+                    <textarea id="antwort2" class="md-textarea form-control"rows="2" placeholder="Antwort..."  name="antwort2-4""
+                           required>$antwort2[1]</textarea>
                 </div>
 ENDE;
             echo $bearbeiten;
 
             if (isset($antwort3)) {
                 $bearbeiten = <<<ENDE
-                <div id="antwort3feld">
-                    <input type="text" class="antwort" id="antwort3" name="antwort3-4" value="$antwort3[1]" placeholder="Antwort 3..." required>
+                <div id="antwort3feld" class="md-form">
+                    <textarea id="antwort3" class="md-textarea form-control"rows="3" placeholder="Antwort..." name="antwort3-4""
+                           required>$antwort3[1]</textarea>
                 </div>
 ENDE;
                 echo $bearbeiten;
             }
             if (isset($antwort4)) {
                 $bearbeiten = <<<ENDE
-                <div id="antwort4feld">
-                    <input type="text" class="antwort" id="antwort4" name="antwort4-4" value="$antwort4[1]" placeholder="Antwort 4..." required>
+                <div id="antwort4feld" class="md-form">
+                    <textarea id="antwort4" class="md-textarea form-control"rows="3" placeholder="Antwort..." name="antwort4-4""
+                           required>$antwort4[1]</textarea>
                 </div>
 ENDE;
                 echo $bearbeiten;
@@ -75,11 +87,16 @@ ENDE;
 
             echo "</div>";
 
-            echo "<button id='senden' name='frageID-4' value='$frageID' type='submit'>Senden</button>";
+            echo "<button class='button' id='button-senden' name='frageID-4' value='$frageID' type='submit'>Senden</button>";
 
             ?>
 
         </form>
+
+        <form method="get" action="../Admin-Login/index2.php">
+            <input type="submit" class="button" id="button-zurueck" value="Zurück">
+        </form>
+
     </div>
 
 </section>
